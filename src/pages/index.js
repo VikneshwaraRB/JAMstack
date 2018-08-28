a503eab8e4c5f212113db51cfe3b52c088796461
VikneshwaraRB <16euit172@skcet.ac.in>
2018-08-28 23:48:24 +0530

    first version of website

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Vikneshwara R B</h1>
+        <p>Hi.Iam pursuing 3rd year CS undergrad course in India.I follow freecodecamp for more than 6 months.Freecodecamp provides more updated content everytime.</p>
+    <p>This is my first JAMstack website.I have decided to get my hands dirty on JAMstack by participating in the hackathon.</p>
+    <p> I will build cool and great projects and hope that this hackathon would be a great platform for me.</p>
+    <Link to="/page-2/">Click here to read about my short description.</Link>
+  </div>
+)
+
+export default IndexPage
---
+++ src/pages/page-2.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const SecondPage = () => (
+  <div>
+    <h1>Short description about me</h1>
+    <p>Iam a tech enthusiast who explores new technologies and work for building solutions</p>
+    <p>I hope that technology can make people wizards with capabilites</p>
+    <Link to="/">Go back</Link>
+  </div>
+)
+
+export default SecondPage
